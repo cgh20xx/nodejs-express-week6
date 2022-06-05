3e61cf478ddd3967533238f119492bc0286ef637
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2022-06-05 17:25:24 +0800

    feat: 新增會員註冊、登入、重設密碼、讀取及修改會員資料 API。

--- routes/posts.js
+++ routes/posts.js
@@ -5,12 +5,16 @@ const handleErrorAsync = require('../services/handleErrorAsync');
 
 // 取得所有貼文
 router.get('/posts', handleErrorAsync(PostsController.getPosts));
+
 // 新增單筆貼文
 router.post('/post', handleErrorAsync(PostsController.createPost));
+
 // 刪除所有貼文
 router.delete('/posts', handleErrorAsync(PostsController.deletePosts));
+
 // 刪除單筆貼文
 router.delete('/post/:id', handleErrorAsync(PostsController.deletePostById));
+
 // 修改單筆貼文
 router.patch('/post/:id', handleErrorAsync(PostsController.updatePostById));
 
